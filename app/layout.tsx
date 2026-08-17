0a813a12e3a89d498fc2114b7ca1a7b038e9255e
Dominance & Delusion <gjnicholsiii@gmail.com>
2026-08-16 22:50:31 -0500

    Load uniform Blackvane 13 header styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./editorial.css";
 import "./editorial-images.css";
+import "./site-header.css";
 import SiteHeader from "./SiteHeader";
 
 export const metadata: Metadata = {
